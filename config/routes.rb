f0b98217773e82509835df6c1470914ec2de952d
Joseph Maloney <j.maloney.ii@gmail.com>
2018-03-14 22:13:42 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root to: 'static_pages#home'
+  
+  get 'static_pages/home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

9eadad4ff0667c7791cb2b042d558d7ab78464a8
Joseph Maloney <j.maloney.ii@gmail.com>
2018-03-24 20:23:22 -0700

    add admin sidebar and styling to make it work.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < UnsecureApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
   def index
+    @posts = Post.all
+  end
+
+  def blog_admin
     @posts = Post.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :admins
+
   resources :posts, path: 'blog'
+  get 'blog_admin', to: 'posts#blog_admin'
+
   root to: 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

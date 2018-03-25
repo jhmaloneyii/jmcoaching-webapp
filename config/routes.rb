a7418a28a2f10f794c03729a1a8e800652908d45
Joseph Maloney <j.maloney.ii@gmail.com>
2018-03-24 22:30:22 -0700

    add tags for blog posts using scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :tags
   devise_for :admins
 
   resources :posts, path: 'blog'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325023901) do
+ActiveRecord::Schema.define(version: 20180325052856) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20180325023901) do
     t.text "summary"
     t.boolean "active"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

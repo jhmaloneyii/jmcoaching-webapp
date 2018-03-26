edd952525024104e40f3e3d53b4108474dad8653
Joseph Maloney <j.maloney.ii@gmail.com>
2018-03-25 18:42:38 -0700

    implement posts and tags controller tests. tags currently failing.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @post = posts(:one)
+    @admin = Admin.create(email: "example@test.com", password: "asdfasdf", username: "MrExample")
   end
 
   test "should get index" do
@@ -11,16 +12,30 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in @admin
     get new_post_url
     assert_response :success
   end
 
+  test "should redirect from new" do
+    get new_post_url
+    assert_response :redirect
+  end
+
   test "should create post" do
+    sign_in @admin
     assert_difference('Post.count') do
       post posts_url, params: { post: { active: @post.active, content: @post.content, image: @post.image, summary: @post.summary, title: @post.title } }
     end
 
     assert_redirected_to post_url(Post.last)
+  end
+
+  test "should redirect from create" do
+    assert_no_difference('Post.count') do
+      post posts_url, params: { post: { active: @post.active, content: @post.content, image: @post.image, summary: @post.summary, title: @post.title } }
+    end
+    assert_response :redirect
   end
 
   test "should show post" do
@@ -29,20 +44,51 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    sign_in @admin
     get edit_post_url(@post)
     assert_response :success
   end
 
+  test "should redirect from edit" do
+    get edit_post_url(@post)
+    assert_response :redirect
+  end
+
   test "should update post" do
+    sign_in @admin
     patch post_url(@post), params: { post: { active: @post.active, content: @post.content, image: @post.image, summary: @post.summary, title: @post.title } }
     assert_redirected_to post_url(@post)
   end
 
+  test "should redirect from update" do
+    patch post_url(@post), params: { post: { active: @post.active, content: @post.content, image: @post.image, summary: @post.summary, title: @post.title } }
+    assert_redirected_to new_admin_session_url
+  end
+
   test "should destroy post" do
+    sign_in @admin
     assert_difference('Post.count', -1) do
       delete post_url(@post)
     end
 
     assert_redirected_to posts_url
+  end
+
+  test "should redirect from destroy post" do
+    assert_no_difference('Post.count') do
+      delete post_url(@post)
+    end
+    assert_redirected_to new_admin_session_url
+  end
+
+  test "should get admin_index" do
+    sign_in @admin
+    get blog_admin_url
+    assert_response :success
+  end
+
+  test "should redirect away from admin_index" do
+    get blog_admin_url
+    assert_redirected_to new_admin_session_url
   end
 end
--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -3,19 +3,23 @@ require 'test_helper'
 class TagsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @tag = tags(:one)
+    @admin = Admin.create(email: "example@test.com", password: "asdfasdf", username: "MrExample")
   end
 
   test "should get index" do
+    sign_in @admin
     get tags_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in @admin
     get new_tag_url
     assert_response :success
   end
 
   test "should create tag" do
+    sign_in @admin
     assert_difference('Tag.count') do
       post tags_url, params: { tag: { name: @tag.name } }
     end
@@ -24,25 +28,53 @@ class TagsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show tag" do
+    sign_in @admin
     get tag_url(@tag)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in @admin
     get edit_tag_url(@tag)
     assert_response :success
   end
 
   test "should update tag" do
+    sign_in @admin
     patch tag_url(@tag), params: { tag: { name: @tag.name } }
     assert_redirected_to tag_url(@tag)
   end
 
   test "should destroy tag" do
+    sign_in @admin
     assert_difference('Tag.count', -1) do
       delete tag_url(@tag)
     end
 
     assert_redirected_to tags_url
+  end
+
+  test "should redirect from all except show" do
+    get tags_url
+    assert_redirected_to new_admin_session_url
+
+    get new_tag_url
+    assert_redirected_to new_admin_session_url
+
+    assert_no_difference('Tag.count') do
+      post tags_url, params: { tag: { name: @tag.name } }
+    end
+    assert_redirected_to new_admin_session_url
+
+    get edit_tag_url
+    assert_redirected_to new_admin_session_url
+
+    patch tag_url(@tag), params: { tag: { name: @tag.name } }
+    assert_redirected_to new_admin_session_url
+
+    assert_no_difference('Tag.count') do
+      delete tag_url(@tag)
+    end
+    assert_redirected_to new_admin_session_url
   end
 end

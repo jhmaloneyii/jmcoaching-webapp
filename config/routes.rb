9a49c0a721684cf82f470bebfd567791da3970b1
Joseph Maloney <j.maloney.ii@gmail.com>
2018-04-15 09:18:42 -0700

    add products scaffolding to begin store.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :products
   resources :tags
 
   devise_for :admins, :skip => [:registrations]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402023536) do
+ActiveRecord::Schema.define(version: 20180413042902) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20180402023536) do
     t.integer "tag_id", null: false
     t.index ["post_id", "tag_id"], name: "index_posts_tags_on_post_id_and_tag_id"
     t.index ["tag_id", "post_id"], name: "index_posts_tags_on_tag_id_and_post_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.float "price"
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|

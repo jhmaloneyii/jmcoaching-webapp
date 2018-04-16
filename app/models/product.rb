f0f40d1e4fac37eb30ee425069a7df414c668c94
Joseph Maloney <j.maloney.ii@gmail.com>
2018-04-15 17:43:04 -0700

    add product type fields to show.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :product_type
   serialize :properties, Hash
+
+  validate :validate_required
+
+  def validate_required
+    product_type.fields.each do |field|
+      if field.required? && properties[field.name].blank?
+        errors.add field.name, "must not be blank"
+      end
+    end
+
+  end
 end

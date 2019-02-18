a0f312fa576a986ec9fdf4f891da68db2709a3ec
Joseph Maloney <j.maloney.ii@gmail.com>
2019-02-18 05:19:29 -0800

    add coach page and page for sam and caleb, with routes and controler methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,14 @@ class StaticPagesController < UnsecureApplicationController
 
   def home
   end
-  
+
+  def coaches
+  end
+
+  def caleb
+  end
+
+  def sam
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'blog_admin', to: 'posts#blog_admin'
 
   root to: 'static_pages#home'
+  get 'coaches', to: 'static_pages#coaches'
+  get 'coaches/caleb', to: 'static_pages#caleb'
+  get 'coaches/sam', to: 'static_pages#sam'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
